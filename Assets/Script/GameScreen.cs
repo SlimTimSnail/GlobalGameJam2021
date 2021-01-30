c1fb8edc5fb3fc7bf695ef9fc8ee7f147158a901
rami <ramivalmont@gmail.com>
2021-01-30 17:23:43 +1300

    work on object selection

--- Assets/Script/GameScreen.cs
+++ Assets/Script/GameScreen.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameScreen : MonoBehaviour
 {
+    public LostObject CurrentSelected = null;
+    public List<LostObject> LostObjects = null;
+    public Button SubmitButton = null;
+    public Button ReturnButton = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(LostObject lo in LostObjects)
+        {
+            lo.OnSelected += OnObjectSelect;
+        }
     }
 
     // Update is called once per frame
@@ -18,5 +27,42 @@ public class GameScreen : MonoBehaviour
         {
             SceneManager.LoadScene("Scene_End");
         }
+    }
+
+    void OnObjectSelect(LostObject lostObject)
+    {
+        if(CurrentSelected != null)
+        {
+            CurrentSelected.Deselect();
+        }
+        CurrentSelected = lostObject;
+        CurrentSelected.Select();
+        SubmitButton.interactable = true;
+        ReturnButton.interactable = true;
+    }
+
+    public void SubmitObject()
+    {
+        if(CurrentSelected.correctObject == true)
+        {
+            Debug.Log("Win");
+        }
+        else if(CurrentSelected.alreadySubmitted == true)
+        {
+            Debug.Log("I said Nope");
+        }
+        else
+        {
+            Debug.Log("Nope");
+            CurrentSelected.alreadySubmitted = true;
+        }
+    }
+
+    public void ReturnObjectToBox()
+    {
+        CurrentSelected.Deselect();
+        CurrentSelected = null;
+        SubmitButton.interactable = false;
+        ReturnButton.interactable = false;
     }
 }
---
+++ Assets/Script/LostObject.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LostObject : MonoBehaviour
+{
+    public event Action<LostObject> OnSelected = null;
+
+    public bool correctObject = false;
+    public bool alreadySubmitted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if (OnSelected != null)
+        {
+            OnSelected.Invoke(this);
+        }
+    }
+
+    public void Select()
+    {
+        Debug.Log("I'm selected");
+    }
+
+    public void Deselect()
+    {
+        Debug.Log("I'm deselected");
+    }
+}
